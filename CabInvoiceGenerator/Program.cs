e84cb1fa126bb43f9d663ca771a3b708656a4d07
dnyana <dnyaneshgawade903@gmail.com>
2021-08-27 10:46:57 +0530

    [Dnyanesh] Add : Creating Function To Calculate Fare

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CabInvoiceGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome To The Cab Invoice Service!!!");
+            InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+            double fare = invoiceGenerator.CalculateFare(13, 24);
+            Console.WriteLine($"Fare : {fare}");
+        }
+    }
+}
